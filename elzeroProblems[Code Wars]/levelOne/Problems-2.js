9929b415ddc8c1ae86804f0ced04a8978cbb49e9
Mohamed ID-ABDELLAH <118572049+id-abdellah@users.noreply.github.com>
2023-05-13 11:57:14 +0100

    Create Problems-2.js

---
+++ elzeroProblems[Code Wars]/levelOne/Problems-2.js
@@ -0,0 +1,35 @@
+/* Even or Odd */
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'even' : 'odd';
+}
+console.log(evenOrOdd(8)); // even
+console.log(evenOrOdd(3)); // odd
+
+
+
+
+/* reverse a string */
+
+function reversString(str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+  // return str.split('').reverse().join(''); // built-in functions
+}
+console.log(reversString('abcd')); // dcba
+
+
+
+
+/* Boolean To String */
+
+function booleanToString(b) {
+  // return String(b);
+  // return `${b}`;
+  return b ? 'true' : 'false';
+}
+
+console.log(booleanToString(false)); // "false"
